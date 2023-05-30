5e05893dfec6728f90dac74a432ceede198e2785
matt manes <mattmanes@pm.me>
2023-05-29 21:34:24 -0500

    perf: decrease execution time and line count with a better matching algo

--- src/dupechecker/dupechecker.py
+++ src/dupechecker/dupechecker.py
@@ -7,55 +7,23 @@ from itertools import combinations
 from griddle import griddy
 from pathier import Pathier
 from printbuddies import Spinner
+from noiftimer import time_it
 
 
-def get_pairs(path: Pathier, recursive: bool = False) -> list[tuple[Pathier, Pathier]]:
-    """Return a list of tuples where each tuple is a unique pair of files in `path` to be compared.
-    If `recursive` is `True`, additional files to be compared will be pulled from subdirectories of `path`"""
+def get_duplicates(path: Pathier, recursive: bool = False) -> list[list[Pathier]]:
+    """Return a list of lists for duplicate files in `path`.
+    Each sub-list will contain 2 or more files determined to be equivalent files.
+    If `recursive` is `True`, files from `path` and it's subdirectories will be compared."""
     files = list(path.rglob("*.*")) if recursive else list(path.glob("*.*"))
-    return list(combinations(files, 2))
-
-
-def compare_files(files: tuple[Pathier, Pathier]) -> bool:
-    """Compare two files, return `True` if they are the same."""
-    return filecmp.cmp(files[0], files[1], False)
-
-
-def get_matches(pairs: list[tuple[Pathier, Pathier]]) -> list[tuple[Pathier, Pathier]]:
-    """Return a list of file pairs where each file in a pair is determined to be the same."""
-    return [pair for pair in pairs if compare_files(pair)]
-
-
-def combine_matches(matches: list[tuple[Pathier, Pathier]]) -> list[list[Pathier]]:
-    """Consolidate equivalent files based on if `a == b` and `b == c`, then also `a == c`.
-
-    i.e. (each tuple in `matches` represents two files determined to be equivalent)
-    >>> matches = [(file1, file2), (file2, file3), (file4, file5), (file6, file1)]
-    >>> combine_matches(matches)
-    >>> [[file1, file2, file3, file6], [file4, file5]]"""
-    combined_matches = []
-    while len(matches) > 0:
-        this_match = matches.pop()
-        combined_match = list(this_match)
-        poppers = []
-        for match in matches:
-            if match[0] in combined_match and match[1] not in combined_match:
-                combined_match.append(match[1])
-                poppers.append(match)
-            elif match[1] in combined_match and match[0] not in combined_match:
-                combined_match.append(match[0])
-                poppers.append(match)
-            elif match[0] in combined_match and match[1] in combined_match:
-                poppers.append(match)
-        combined_matches.append(combined_match)
-        for popper in poppers:
-            matches.pop(matches.index(popper))
-    return combined_matches
-
-
-# Wrapping these two steps in one function for threading in dupechecker()
-def process_files(pairs: list[tuple[Pathier, Pathier]]) -> list[list[Pathier]]:
-    return combine_matches(get_matches(pairs))
+    matching_sets = []
+    while len(files) > 0:
+        comparee = files.pop()
+        matching_files = [file for file in files if filecmp.cmp(comparee, file, False)]
+        if matching_files:
+            [files.pop(files.index(file)) for file in matching_files]
+            matching_files.insert(0, comparee)
+            matching_sets.append(matching_files)
+    return matching_sets
 
 
 def get_args() -> argparse.Namespace:
@@ -106,11 +74,10 @@ def delete_wizard(matches: list[list[Pathier]]):
             [map_[num].delete() for num in map_ if num != keeper]
 
 
+@time_it()
 def dupechecker(args: argparse.Namespace | None = None):
     if not args:
         args = get_args()
-    pairs = get_pairs(args.path, args.recursive)
-    print(f"Comparing {len(pairs)} pairs of files for duplicates...")
     s = [
         ch.rjust(i + j)
         for i in range(1, 25, 3)
@@ -119,13 +86,13 @@ def dupechecker(args: argparse.Namespace | None = None):
     s += s[::-1]
     with Spinner(s) as spinner:
         with ThreadPoolExecutor() as exc:
-            thread = exc.submit(process_files, pairs)
+            thread = exc.submit(get_duplicates, args.path, args.recursive)
             while not thread.done():
                 spinner.display()
                 time.sleep(0.025)
             matches = thread.result()
     if matches:
-        print(f"Found {len(matches)} duplicate files:")
+        print(f"Found {len(matches)} duplicate sets of files:")
         print(griddy(matches))
         if args.delete_dupes:
             delete_wizard(matches)
