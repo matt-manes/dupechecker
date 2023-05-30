9793040d9edf46154299b4c6f8e8e6ec767823f4
matt manes <mattmanes@pm.me>
2023-05-29 20:01:15 -0500

    docs: add doc strings to functions

--- src/dupechecker/dupechecker.py
+++ src/dupechecker/dupechecker.py
@@ -1,35 +1,45 @@
 import argparse
 import filecmp
-from itertools import combinations
-from concurrent.futures import ThreadPoolExecutor
 import time
+from concurrent.futures import ThreadPoolExecutor
+from itertools import combinations
 
 from griddle import griddy
 from noiftimer import time_it
 from pathier import Pathier
-from printbuddies import Spinner, clear
+from printbuddies import Spinner
 
 
 def get_pairs(path: Pathier, recursive: bool = False) -> list[tuple[Pathier, Pathier]]:
+    """Return a list of tuples where each tuple is a unique pair of files in `path` to be compared.
+    If `recursive` is `True`, additional files to be compared will be pulled from subdirectories of `path`"""
     files = list(path.rglob("*.*")) if recursive else list(path.glob("*.*"))
     return list(combinations(files, 2))
 
 
 def compare_files(files: tuple[Pathier, Pathier]) -> bool:
+    """Compare two files, return `True` if they are the same."""
     return filecmp.cmp(files[0], files[1], False)
 
 
 def get_matches(pairs: list[tuple[Pathier, Pathier]]) -> list[tuple[Pathier, Pathier]]:
+    """Return a list of file pairs where each file in a pair is determined to be the same."""
     return [pair for pair in pairs if compare_files(pair)]
 
 
 def combine_matches(matches: list[tuple[Pathier, Pathier]]) -> list[list[Pathier]]:
+    """Consolidate equivalent files based on if `a == b` and `b == c`, then also `a == c`.
+
+    i.e. (each tuple in `matches` represents two files determined to be equivalent)
+    >>> matches = [(file1, file2), (file2, file3), (file4, file5), (file6, file1)]
+    >>> combine_matches(matches)
+    >>> [[file1, file2, file3, file6], [file4, file5]]"""
     combined_matches = []
     while len(matches) > 0:
         this_match = matches.pop()
         combined_match = list(this_match)
         poppers = []
-        for j, match in enumerate(matches):
+        for match in matches:
             if match[0] in combined_match and match[1] not in combined_match:
                 combined_match.append(match[1])
                 poppers.append(match)
@@ -42,6 +52,7 @@ def combine_matches(matches: list[tuple[Pathier, Pathier]]) -> list[list[Pathier
     return combined_matches
 
 
+# Wrapping these two steps in one function for threading in dupechecker()
 def process_files(pairs: list[tuple[Pathier, Pathier]]) -> list[list[Pathier]]:
     return combine_matches(get_matches(pairs))
 
@@ -83,14 +94,13 @@ def dupechecker(args: argparse.Namespace | None = None):
         for j, ch in enumerate(["/", "-", "\\"])
     ]
     s += s[::-1]
-    spinner = Spinner(s)
-    with ThreadPoolExecutor() as exc:
-        thread = exc.submit(process_files, pairs)
-        while not thread.done():
-            spinner.display()
-            time.sleep(0.025)
-        matches = thread.result()
-    clear()
+    with Spinner(s) as spinner:
+        with ThreadPoolExecutor() as exc:
+            thread = exc.submit(process_files, pairs)
+            while not thread.done():
+                spinner.display()
+                time.sleep(0.025)
+            matches = thread.result()
     if matches:
         print(f"Found {len(matches)} duplicate files:")
         print(griddy(matches))
